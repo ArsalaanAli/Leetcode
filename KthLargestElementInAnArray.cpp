41b1c7a95c803b2b8d834749b4e85e3364cd9a92
arsalaanAli <38818326+ArsalaanAli@users.noreply.github.com>
2024-01-27 16:35:43 -0500

    lc 215 quick select

--- KthLargestElementInAnArray.cpp
+++ KthLargestElementInAnArray.cpp
@@ -11,3 +11,36 @@ public:
         return pq.top();
     }
 };
+
+/*
+SOLUTION WITH QUICK SELECT ALGORITHM (DOESNT WORK ON LEETCODE ANYMORE)
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        int r = nums.size()-1;
+        int l = 0;
+        int part = l;
+        int target = nums.size()-k;
+        while(true){
+            for(int i = l; i<r; i++){
+                if(nums[i]<nums[r]){
+                    swap(nums[i], nums[part]);
+                    part++;
+                }
+            }
+            swap(nums[r], nums[part]);
+            if(part == target){
+                break;
+            }
+            if(part<target){
+                part++;
+                l = part;
+            }
+            else{
+                r = part-1;
+                part = l;
+            }
+        }
+        return nums[part];
+    }
+};*/
